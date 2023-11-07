845629990a02ad864900188ecdd9d461d65f8384
PAVAN KUMAR <kpavankumar2410@gmail.com>
2023-11-07 09:20:16 -0500

    Making shell scripting topics-add underline

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-sudo cd /usr/share/nginx/html
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 systemctl enable nginx
